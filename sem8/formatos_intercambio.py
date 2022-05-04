50bcf296f6a3bd7589702282cf7ddea262d8fc79
mundostr <idux.net@gmail.com>
2022-05-04 19:28:46 -0300

    Primer ej JSON

---
+++ sem8/formatos_intercambio.py
@@ -0,0 +1,19 @@
+"""
+Repaso formatos interecambio habituales
+
+txt = texto plano
+csv = valores separados por coma (comma separated values)
+rss = RSS (RSS feed)
+xml = XML (Extended Markup Language)
+json = JSON (JavaScript Object Notation)
+
+Ej json:
+
+{
+    "nombre": "Juan",
+    "apellido": "Perez",
+    "edad": "30",
+    "saldo": "328500.5",
+    "estado": "activo"
+}
+"""
