f63ca4e6ec83e256678ec60a8da26585fe800b1c
mundostr <idux.net@gmail.com>
2022-05-11 18:36:32 -0300

    Agregado control main

---
+++ sem9/control_main.py
@@ -0,0 +1,7 @@
+CONSTANTE = float(15.68)
+
+
+# Este pequeño control nos permite verificar si el código está siendo llamado de manera directa o no
+if (__name__ == "__main__"):
+	print("Este script está siendo ejecutado de manera directa")
+	print(CONSTANTE)
---
+++ sem9/import_control_main.py
@@ -0,0 +1,4 @@
+import control_main as cfg
+
+print("Este es otro archivo que importa control_main.py")
+print(cfg.CONSTANTE)
