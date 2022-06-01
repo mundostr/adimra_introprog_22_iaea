05127c10867d4ecd3ab0b07e7b5be2827386678a
mundostr <idux.net@gmail.com>
2022-06-01 19:54:00 -0300

    Solicitud remota funcional

--- sem12/solicitud_remota.py
+++ sem12/solicitud_remota.py
@@ -17,15 +17,32 @@ import requests
 NOMBRE_CIUDAD = "rafaela"
 UNIDADES = "metric"
 APPID = "b07cceb706b36724ddaa2614cdb76af4"
-IDIOMA = "en"
+IDIOMA = "es"
 
-URL = f"https://api.openweathermap.org/data/2.5/weather?q={NOMBRE_CIUDAD}&units={UNIDADES}&appid={APPID}&lang={IDIOMA}"
+URL = f"https://api.openweathermap.org/data/2.5/weather2?q={NOMBRE_CIUDAD}&units={UNIDADES}&appid={APPID}&lang={IDIOMA}"
 
 
-solicitud = requests.get(URL)
-solicitud_json = solicitud.json()
+def solicitar_datos_clima(ruta):
+    proceso = requests.get(ruta)
 
-print(type(solicitud_json))
-print(solicitud_json["main"])
-print(solicitud_json["main"]["temp"])
-print(solicitud_json["weather"][0]["description"])
+    if (proceso.status_code == 200):
+        return proceso.json()
+    else:
+        return False
+
+
+def mostrar_datos_clima(datos):
+    print(datos["main"]["temp"])
+    print(datos["weather"][0]["description"])
+
+
+def principal():
+    datos_json = solicitar_datos_clima(URL)
+    if (datos_json == False):
+        print("Error al solicitar datos")
+    else:
+        mostrar_datos_clima(datos_json)
+
+
+if (__name__ == "__main__"):
+    principal()
