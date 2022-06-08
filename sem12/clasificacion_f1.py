3fe686a088d499c3896675d90aff9dd377abf39b
mundostr <idux.net@gmail.com>
2022-06-08 18:03:16 -0300

    Ejemplos clasif f1 backup local

---
+++ sem12/clasificacion_f1.py
@@ -0,0 +1,29 @@
+import json
+
+import requests
+
+RUTA_LOCAL = "sem12/clasificacion_f1.json"
+RUTA_REMOTA = "https://ergast.com/api/f1/2022/driverStandings.json"
+
+
+def guardar_backup_local(ruta, contenido):
+	with open(ruta, "w", encoding="UTF-8") as archivo:
+		archivo.write(json.dumps(contenido))
+	print("Backup local guardado")
+
+def recuperar_clasificacion_remota(ruta):
+    proceso = requests.get(ruta)
+
+    if (proceso.status_code == 200):
+        return proceso.json()
+    else:
+        return False
+
+def main():
+	clasificacion = recuperar_clasificacion_remota(RUTA_REMOTA)
+	## guardar_backup_local(RUTA_LOCAL, json.dumps(clasificacion))
+	guardar_backup_local(RUTA_LOCAL, clasificacion)
+
+
+if __name__ == "__main__":
+	main()
