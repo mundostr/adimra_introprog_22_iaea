3f7c1af915421724765d5afb30c9c1ad5f0b30c4
mundostr <idux.net@gmail.com>
2022-06-29 20:13:12 -0300

    Mysql base

--- sem16/mysql_base.py
+++ sem16/mysql_base.py
@@ -13,5 +13,6 @@ def consultar_bbdd(consulta):
 if (__name__ == "__main__"):
 	# CONSULTA = "select * from sensores order by id asc"
 	CONSULTA = "select sensores.nombre, lecturas.media from sensores, lecturas where sensores.id = lecturas.id_sensor and sensores.id = 32 order by sensores.id asc"
+	
 	resultado = consultar_bbdd(CONSULTA)
 	print(resultado);
